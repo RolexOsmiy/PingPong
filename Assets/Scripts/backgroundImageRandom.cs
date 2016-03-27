34e2719fa2133d21c7596786e0ac30d588dada8f
MaximReshetnikovFWS <10maxenergy@gmail.com>
2016-03-27 23:23:36 +0500

    New menu. Music. SP menu.

---
+++ Assets/Scripts/backgroundImageRandom.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundImageRandom : MonoBehaviour {
+
+	public Sprite[] mSpriteImage = new Sprite[20];
+	public int maxSpriteImage = 20;
+	private int numberImage;
+	// Use this for initialization
+	void Start () {
+		numberImage = Random.Range (0,maxSpriteImage);
+		gameObject.GetComponent<SpriteRenderer>().sprite = mSpriteImage[numberImage];
+	}
+
+}
---
+++ Assets/rotateThis.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateThis : MonoBehaviour {
+	public float rotatePerSecondx = 0f;
+	public float rotatePerSecondy = 0.5f;
+	public float rotatePerSecondz = 0f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (rotatePerSecondx * Time.deltaTime,rotatePerSecondy * Time.deltaTime,rotatePerSecondz * Time.deltaTime);
+	}
+}
